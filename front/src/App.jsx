ef094a97c946537ac1ba8de08022576de19d01c1
leftinant <benjaminyoungner@gmail.com>
2025-07-05 14:28:22 +0300

    initial commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/navBar";
 import Banner from "./components/banner";
 import Footer from "./components/Footer";
+import TopBooks from "./components/TopBooks";
 
 function App () {
   return (
   <>
     <NavBar/>
     <Banner/>
+    <TopBooks/>
     <Footer/>
   </>
   );
