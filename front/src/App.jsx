857cd2e5403e5743e67328ec3b8ac0967b5ef5da
leftinant <benjaminyoungner@gmail.com>
2025-07-05 11:08:14 +0300

    initial commit

---
+++ front/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./components/navBar";
+import Banner from "./components/banner";
+import Footer from "./components/Footer";
+
+function App () {
+  return (
+  <>
+    <NavBar/>
+    <Banner/>
+    <Footer/>
+  </>
+  );
+}
+
+export default App;
